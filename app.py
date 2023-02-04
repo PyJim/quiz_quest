bdce7621929cd50b98532d62eeaf9489fa8ae1e4
Essel Jimmy Kodwo <jimmy.essel@kibo.school>
2023-02-04 20:06:09 +0000

    comments

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
 
+# dictionary of questions to be used
 """questions = [
     {
         'question': 'Which team did Lionel Messi join in the summer of 2021?',
@@ -19,24 +20,31 @@ app = Flask(__name__)
     # Add more questions here
 ]"""
 
-
+# url of API containing the questions
 API_URL = "https://opentdb.com/api.php?amount=20&category=9&difficulty=easy&type=multiple"
 
+# http request to API
 data = requests.get(url=API_URL)
+
+# converting API data to json
 database = data.json()["results"]
 
+# filtering API results to suit the context
 questions = [{key: item[key] for key in item.keys()
               & {'question', 'correct_answer', 'incorrect_answers'}} for item in database]
 
+# total number of questions from the API. 20 in this case
 number_of_questions = len(questions)
 
+# user score
 score = 0
 
+# home route
 @app.route('/')
 def index():
     return render_template('index.html')
     
-
+# route to quiz page
 @app.route('/quiz/<int:question_id>', methods=['GET', 'POST'])
 def quiz(question_id):
     if request.method == 'POST':
@@ -56,7 +64,7 @@ def quiz(question_id):
 
         return render_template('quiz.html', question=questions[question_id], question_id=int(question_id), number_of_questions=number_of_questions, options=options)
 
-
+# route to final results page
 @app.route('/quiz/final_results', methods=['GET'])
 def final_results():
     return render_template('final_results.html', score=score)
